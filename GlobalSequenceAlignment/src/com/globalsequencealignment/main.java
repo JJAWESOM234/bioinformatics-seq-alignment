42d55e9a3e03642130092cd9496eeb54587f9c78
Jedadiah McFarland <49107280+JJAWESOM234@users.noreply.github.com>
2021-11-06 21:39:44 -0500

    Update main.java
    Add inline comments JavaDoc will be implemented in next phase

--- GlobalSequenceAlignment/src/com/globalsequencealignment/main.java
+++ GlobalSequenceAlignment/src/com/globalsequencealignment/main.java
@@ -15,8 +15,13 @@ public class main
 	 */
 	public static void main(String[] args)
 	{
+		// Create a new GlobalAlignment class instance to gain its functionality
 		GlobalAlignment ga = new GlobalAlignment();
+		
+		// Print the sequence matrix to show scoring 
 		ga.printSequence();
+		
+		// Get the optimally aligned sequences and print them
 		String[] a = ga.getSequences();
 		System.out.println(a[0]);
 		System.out.println(a[1]);
