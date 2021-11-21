d6860c60b087377473c0120005e5ae059cf924d3
Jedadiah McFarland <jedmcfarland715@gmail.com>
2021-11-21 17:32:40 -0600

    Update GlobalAlignment.java
    Update scoring matrix,
    add U - Uracil to support RNA sequences
    changed misspelled words on display

--- GlobalSequenceAlignment/src/com/globalsequencealignment/GlobalAlignment.java
+++ GlobalSequenceAlignment/src/com/globalsequencealignment/GlobalAlignment.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class GlobalAlignment
 {
-	// Initilize gap penalties 
-	private final double GAP_OPEN_PENALTY = -1.0;
+	// Initilize gap penalties
+	private final double GAP_OPEN_PENALTY = -10.0;
 	private final double GAP_EXTEND_PENALTY = 0.5;
 
-	// Initilize user input variables 
+	// Initilize user input variables
 	private char[] sequence1Char;
 	private char[] sequence2Char;
-	
-	// Global Scanner for user input 
+
+	// Global Scanner for user input
 	Scanner scan;
-	
-	// Matrices to track scoring of matches or mismatchs and the overall score of each alignment
+
+	// Matrices to track scoring of matches or mismatchs and the overall score of
+	// each alignment
 	private int[][] scoringMatrix;
 	private int[][] sequenceMatrix;
 
@@ -23,14 +24,14 @@ public class GlobalAlignment
 	public GlobalAlignment()
 	{
 		this.scan = new Scanner(System.in);
-		
+
 		// Prompts the user for DNA/RNA sequence
 		this.getUserSequences();
-		
+
 		// Sets the scoring matrix to a 2 match and -1 mismatch
 		this.setScoringMatrix();
-		
-		// Store the alignment matrix in the global variable for analyzation 
+
+		// Store the alignment matrix in the global variable for analyzation
 		this.sequenceMatrix = this.fillSequenceMatrix();
 	}
 
@@ -42,21 +43,21 @@ public class GlobalAlignment
 
 	protected void getUserSequence1()
 	{
-		System.out.print("Enter the first DNA seqeunce: ");
+		System.out.print("Enter the first DNA sequence: ");
 		this.sequence1Char = this.scan.next().toCharArray();
 		this.validateSequence1();
 	}
 
 	protected void getUserSequence2()
 	{
-		System.out.print("Enter the second DNA seqeunce: ");
+		System.out.print("Enter the second DNA sequence: ");
 		this.sequence2Char = this.scan.next().toCharArray();
 		this.validateSequence2();
 	}
 
 	protected int[][] fillSequenceMatrix()
 	{
-		// Create a temp matrix that will be populated	
+		// Create a temp matrix that will be populated
 		int[][] seqMatrix = new int[this.sequence1Char.length + 1][this.sequence2Char.length + 1];
 
 		// Loop through the rows and set the cell value to gap penalty * rows index
@@ -65,28 +66,29 @@ public class GlobalAlignment
 			seqMatrix[i][0] = i * (int) this.GAP_OPEN_PENALTY;
 		}
 
-		// Loop through the columns and set the cell value to gap penalty * coulmns index
+		// Loop through the columns and set the cell value to gap penalty * coulmns
+		// index
 		for (int i = 0; i < seqMatrix[0].length; i++)
 		{
 			seqMatrix[0][i] = i * (int) this.GAP_OPEN_PENALTY;
 		}
 
-		// Loop through the entire matrix 
+		// Loop through the entire matrix
 		for (int i = 1; i < seqMatrix.length; i++)
 		{
 			for (int j = 1; j < seqMatrix[0].length; j++)
 			{
-				// Gets the scoring values for top-left, left, and top cells 
-				
-				// Diagonal represent match or mismatch 
+				// Gets the scoring values for top-left, left, and top cells
+
+				// Diagonal represent match or mismatch
 				int match = seqMatrix[i - 1][j - 1]
 						+ this.getValues(this.sequence1Char[i - 1], this.sequence2Char[j - 1]);
-				
-				// Left or Top represents the deletion of a residue and insertion of a gap 
+
+				// Left or Top represents the deletion of a residue and insertion of a gap
 				int delete = seqMatrix[i - 1][j] + (int) this.GAP_OPEN_PENALTY;
 				int insert = seqMatrix[i][j - 1] + (int) this.GAP_OPEN_PENALTY;
 
-				// Set the current cells value to the maximum of the three computed scores 
+				// Set the current cells value to the maximum of the three computed scores
 				seqMatrix[i][j] = this.max(match, delete, insert);
 			}
 		}
@@ -96,13 +98,14 @@ public class GlobalAlignment
 
 	protected String[] getSequences()
 	{
-		//  Initilize return sequences and index variables 
+		// Initilize return sequences and index variables
 		String returnSeq1 = "";
 		String returnSeq2 = "";
 		int i = this.sequenceMatrix.length - 1;
 		int j = this.sequenceMatrix[0].length - 1;
-		
-		// loop through the sequence matrix to find the optimal path (represents the alignment) 
+
+		// loop through the sequence matrix to find the optimal path (represents the
+		// alignment)
 		while (i != 0 && j != 0)
 		{
 			// Diagonal case, where both resides are kept (match/mismatch)
@@ -114,14 +117,16 @@ public class GlobalAlignment
 				i -= 1;
 				j -= 1;
 			}
-			// Left case, where the 2nd sequence residue remains and a gap is inserted into the 1st sequence position
+			// Left case, where the 2nd sequence residue remains and a gap is inserted into
+			// the 1st sequence position
 			else if (this.sequenceMatrix[i][j - 1] == this.sequenceMatrix[i][j] - this.GAP_OPEN_PENALTY)
 			{
 				returnSeq1 = "-" + returnSeq1;
 				returnSeq2 = this.sequence2Char[j - 1] + returnSeq2;
 				j -= 1;
 			}
-			// Top case, where the 1st sequence residue remains and a gap is inserted into the 2nd sequence position
+			// Top case, where the 1st sequence residue remains and a gap is inserted into
+			// the 2nd sequence position
 			else
 			{
 				returnSeq1 = this.sequence1Char[i - 1] + returnSeq1;
@@ -129,8 +134,9 @@ public class GlobalAlignment
 				i -= 1;
 			}
 		}
-		
-		// If one of the sequences is out of resiude before the other this fills in the rest of the sequence with gaps 
+
+		// If one of the sequences is out of resiude before the other this fills in the
+		// rest of the sequence with gaps
 		if (i == 0)
 		{
 			for (int k = 0; k < j; k++)
@@ -154,10 +160,10 @@ public class GlobalAlignment
 
 	protected int getValues(char a, char b)
 	{
-		// String to show the possible nucleotides (will add 'U') 
-		String nucleotideSeq = "CTAG";
-		
-		// Getting the index which corresponds with the row/column of the scoring matrix 
+		// String to show the possible nucleotides (will add 'U')
+		String nucleotideSeq = "ATGCU";
+
+		// Getting the index which corresponds with the row/column of the scoring matrix
 		int index1 = nucleotideSeq.indexOf(a);
 		int index2 = nucleotideSeq.indexOf(b);
 		return this.scoringMatrix[index1][index2];
@@ -171,8 +177,8 @@ public class GlobalAlignment
 			// Normalizing the capatalization
 			char tempChar = Character.toUpperCase(this.sequence1Char[i]);
 
-			// Check to see if the character is in the valid list 
-			if (tempChar != 'A' && tempChar != 'C' && tempChar != 'G' && tempChar != 'T')
+			// Check to see if the character is in the valid list
+			if (tempChar != 'A' && tempChar != 'C' && tempChar != 'G' && tempChar != 'T' && tempChar != 'U')
 			{
 				// Make the user retry the input if validations fail
 				System.out.println("Invalid Sequence Entered.\n");
@@ -189,9 +195,9 @@ public class GlobalAlignment
 		{
 			// Normalizing the capatalization
 			char tempChar = Character.toUpperCase(this.sequence2Char[i]);
-			
-			// Check to see if the character is in the valid list 
-			if (tempChar != 'A' && tempChar != 'C' && tempChar != 'G' && tempChar != 'T')
+
+			// Check to see if the character is in the valid list
+			if (tempChar != 'A' && tempChar != 'C' && tempChar != 'G' && tempChar != 'T' && tempChar != 'U')
 			{
 				// Make the user retry the input if validations fail
 				System.out.println("Invalid Sequence Entered.\n");
@@ -203,51 +209,56 @@ public class GlobalAlignment
 
 	protected void setScoringMatrix()
 	{
-		// This is the scoring matrix used for the program 
+		// This is the scoring matrix used for the program
+		// ATGCU
 		int[][] tempMatrix =
 		{
-				{ 2, -1, -1, -1 },
-				{ -1, 2, -1, -1 },
-				{ -1, -1, 2, -1 },
-				{ -1, -1, -1, 2 } };
+				{ 5, -4, -4, -4, -4 },
+				{ -4, 5, -4, -4, -4 },
+				{ -4, -4, 5, -4, -4 },
+				{ -4, -4, -4, 5, -4 },
+				{ -4, -4, -4, -4, 5 } };
 
 		this.scoringMatrix = tempMatrix;
 	}
 
 	protected void printSequence()
 	{
-		// Spacing use
-		System.out.printf("%10c", ' ');
-		
-		// Loop through the 2nd sequence to show the residue with each column
-		for (int i = 0; i < this.sequence2Char.length; i++)
+		if (this.sequence1Char.length <= 20 && this.sequence2Char.length <= 20)
 		{
-			System.out.printf("%5c", this.sequence2Char[i]);
-		}
-		// Spacing use
-		System.out.println("");
+			// Spacing use
+			System.out.printf("%10c", ' ');
 
-		// Loop through the sequence matrix and print the scores 
-		for (int i = 0; i < this.sequenceMatrix.length; i++)
-		{
-			// if/else prints the 1st sequence to show the residue with each row
-			if (i != 0)
+			// Loop through the 2nd sequence to show the residue with each column
+			for (int i = 0; i < this.sequence2Char.length; i++)
 			{
-				System.out.printf("%5c", this.sequence1Char[--i]);
-				i++;
+				System.out.printf("%5c", this.sequence2Char[i]);
 			}
-			else
-			{
-				System.out.printf("%5c", ' ');
-			}
-			
-			for (int j = 0; j < this.sequenceMatrix[i].length; j++)
-			{
-				System.out.printf("%5d", this.sequenceMatrix[i][j]);
-			}
+			// Spacing use
 			System.out.println("");
+
+			// Loop through the sequence matrix and print the scores
+			for (int i = 0; i < this.sequenceMatrix.length; i++)
+			{
+				// if/else prints the 1st sequence to show the residue with each row
+				if (i != 0)
+				{
+					System.out.printf("%5c", this.sequence1Char[--i]);
+					i++;
+				}
+				else
+				{
+					System.out.printf("%5c", ' ');
+				}
+
+				for (int j = 0; j < this.sequenceMatrix[i].length; j++)
+				{
+					System.out.printf("%5d", this.sequenceMatrix[i][j]);
+				}
+				System.out.println("");
+			}
 		}
-		// Print the score (bottom right cell value) 
+		// Print the score (bottom right cell value)
 		System.out.printf("\nEstimated Score: %d\n",
 				this.sequenceMatrix[this.sequenceMatrix.length - 1][this.sequenceMatrix[0].length - 1]);
 	}
